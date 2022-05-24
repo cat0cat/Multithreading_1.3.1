89f553f406f83e015a4a820202ed05f7197a584c
Natalia Zubareva <n.zubareva@inbox.ru>
2022-05-24 08:10:26 +0300

    fixed: toy doesn't sleep

--- src/Box.java
+++ src/Box.java
@@ -1,12 +1,10 @@
 public class Box {
     private volatile boolean toggle;
     private static final int PLAYER_SLEEP = 2000;
-    private static final int TOY_SLEEP = 1000;
     private static final int OPEN_LIMIT = 10;
     private int currentOpen = 0;
 
     public void on() {
-
         while (currentOpen < OPEN_LIMIT) {
             if (!toggle) {
                 toggle = true;
@@ -28,11 +26,6 @@ public class Box {
                 toggle = false;
                 System.out.println(Thread.currentThread().getName() + " выключил тумблер и закрыл коробку");
                 currentOpen++;
-                try {
-                    Thread.sleep(TOY_SLEEP);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
             }
         }
     }
